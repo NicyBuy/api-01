5ff33e91de7e92dc75295845add1a7063ef5d1c9
Frank <frankortiz849@gmail.com>
2021-05-08 15:57:22 -0600

    set to true

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -56,7 +56,8 @@ var controller = {
             const token = jwt.sign({id: user._id}, process.env.SECRET, {
                 expiresIn: 60*60*24
             });
-            res.header('Access-Control-Allow-Origin', 'https://koosapp.herokuapp.com');
+            //res.header('Access-Control-Allow-Origin', 'https://koosapp.herokuapp.com');
+            res.set('Access-Control-Allow-Origin', 'https://koosapp.herokuapp.com')
             next();
             //res.status(200).send({message: 'entrado', valid:true, token:token});
             res.json({message: 'alright, entra', auth: true, token});
