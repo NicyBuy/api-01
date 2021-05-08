cd1b21b6701cf032fac87f911557ed2f3b02ca58
Frank <frankortiz849@gmail.com>
2021-05-08 11:30:48 -0600

    new cors added 3

--- src/app.js
+++ src/app.js
@@ -5,6 +5,18 @@ let app = express();
 const cors = require('cors');
 const path =  require('path');
 
+
+// Configurar cabeceras y cors-------------------------------------------------
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');//en lugar del asterisco irian los dominios permitidos
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Credentials', 'true');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+ }); 
+ //app.use(cors({credentials: true, origin: 'http://localhost:3000'}));
+
 //archivos de rutas-------------------------------------------------
 let project_routes = require('./routes/product');
 let store_routes = require('./routes/store');
@@ -31,16 +43,7 @@ var corsOptions = {
 }
 app.use(cors(corsOptions));
 
-// Configurar cabeceras y cors-------------------------------------------------
-app.use((req, res, next) => {
-   res.header('Access-Control-Allow-Origin', '*');//en lugar del asterisco irian los dominios permitidos
-   res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
-   res.header('Access-Control-Allow-Credentials', 'true');
-   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-   res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
-   next();
-}); 
-//app.use(cors({credentials: true, origin: 'http://localhost:3000'}));
+
 
 //RUTAS-------------------------------------------------
 app.use(cors());
