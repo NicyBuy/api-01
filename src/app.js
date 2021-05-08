a4a34978c9d24562d056960146830cf0dffdf94e
Frank <frankortiz849@gmail.com>
2021-05-08 14:34:50 -0600

    1

--- src/app.js
+++ src/app.js
@@ -17,7 +17,7 @@ const path =  require('path');
  }); */
  app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', 'http://localhost:3000, https://koosapp.herokuapp.com');//en lugar del asterisco irian los dominios permitidos
-    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Headers', 'Access-Control-Allow-Origin,Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
     res.header('Access-Control-Allow-Credentials', 'true');
     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
     res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
