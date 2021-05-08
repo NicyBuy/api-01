94ea6d419d62ddef1bafa038399e54c86492674a
Frank <frankortiz849@gmail.com>
2021-05-08 09:49:22 -0600

    new cors added

--- src/app.js
+++ src/app.js
@@ -40,6 +40,7 @@ app.use((req, res, next) => {
    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
    next();
 }); 
+app.use(cors({credentials: true, origin: 'localhost:3000'}));
 
 //RUTAS-------------------------------------------------
 app.use(cors());
