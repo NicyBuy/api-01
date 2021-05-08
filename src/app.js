7d31ffb1669260a49bc1f673f2a7724f66dcf463
Frank <frankortiz849@gmail.com>
2021-05-07 22:38:26 -0600

    otra mas

--- src/app.js
+++ src/app.js
@@ -35,6 +35,7 @@ app.use(cors(corsOptions));
 app.use((req, res, next) => {
    res.header('Access-Control-Allow-Origin', 'http://localhost:3000');//en lugar del asterisco irian los dominios permitidos
    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+   res.header('Access-Control-Allow-Credentials', 'true');
    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
    next();
